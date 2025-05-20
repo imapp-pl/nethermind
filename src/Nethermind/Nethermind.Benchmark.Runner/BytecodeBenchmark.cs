5a7cfe4385724ca9a38cd1ff6fdc6d50649eb914
Jacek Glen <jacek.glen@gmail.com>
2025-05-20 11:09:42 +0200

    Merge main

--- src/Nethermind/Nethermind.Benchmark.Runner/BytecodeBenchmark.cs
+++ src/Nethermind/Nethermind.Benchmark.Runner/BytecodeBenchmark.cs
@@ -7,6 +7,8 @@ using Nethermind.Core;
 using Nethermind.Core.Crypto;
 using Nethermind.Core.Extensions;
 using Nethermind.Core.Specs;
+using Nethermind.Core.Test;
+using Nethermind.Core.Test.Builders;
 using Nethermind.Db;
 using Nethermind.Evm.CodeAnalysis;
 using Nethermind.Specs;
@@ -14,6 +16,7 @@ using Nethermind.Evm.Tracing;
 using Nethermind.Int256;
 using Nethermind.Logging;
 using Nethermind.State;
+using Nethermind.Trie;
 using Nethermind.Trie.Pruning;
 using Nethermind.Evm;
 using Ethereum.Test.Base;
@@ -21,7 +24,18 @@ using static Nethermind.Evm.VirtualMachine;
 using NSubstitute.Routing.AutoValues;
 
 
+using System.Collections.Generic;
+using System.Diagnostics;
+using Nethermind.Config;
+using Nethermind.Consensus.Validators;
+using Nethermind.Core.Test.Modules;
+using Nethermind.Crypto;
+using Nethermind.Evm.TransactionProcessing;
+using Nethermind.Specs.Forks;
+using Nethermind.Specs.Test;
+
 namespace Nethermind.Benchmark.Runner;
+
 public class BytecodeBenchmark
 {
     public static byte[] ByteCode { get; set; }
@@ -38,24 +52,27 @@ public class BytecodeBenchmark
     [GlobalSetup]
     public void GlobalSetup()
     {
-        TrieStore trieStore = new(new MemDb(), new OneLoggerLogManager(NullLogger.Instance));
-        IKeyValueStore codeDb = new MemDb();
+        TrieStore trieStore = TestTrieStoreFactory.Build(new MemDb(),
+                Prune.WhenCacheReaches(1.MiB()),
+                Persist.EveryNBlock(2), NullLogManager.Instance);
+        var codeDb = new MemDb();
 
         _stateProvider = new WorldState(trieStore, codeDb, new OneLoggerLogManager(NullLogger.Instance));
         _stateProvider.CreateAccount(Address.Zero, 1000.Ether());
         _stateProvider.Commit(_spec);
 
-        CodeInfoRepository codeInfoRepository = new();
+        _virtualMachine = new VirtualMachine(_blockhashProvider, MainnetSpecProvider.Instance, LimboLogs.Instance);
 
-        var specProvider = new TestSpecProvider(_spec);
-        _virtualMachine = new VirtualMachine(_blockhashProvider, specProvider, codeInfoRepository, LimboLogs.Instance);
+        
     }
 
     [IterationSetup]
     public void Setup()
     {
         ByteCode = Bytes.FromHexString(Environment.GetEnvironmentVariable("NETH.BENCHMARK.BYTECODE") ?? string.Empty);
-
+        CodeInfoRepository codeInfoRepository = new();
+        KzgPolynomialCommitments.InitializeAsync().Wait();
+        
         _environment = new ExecutionEnvironment
         (
             executingAccount: Address.Zero,
@@ -64,17 +81,17 @@ public class BytecodeBenchmark
             codeInfo: new CodeInfo(ByteCode),
             value: 0,
             transferValue: 0,
-            txExecutionContext: new TxExecutionContext(_header, Address.Zero, 0, null),
+            txExecutionContext: new TxExecutionContext(new BlockExecutionContext(_header, _spec), Address.Zero, 0, null, codeInfoRepository),
             inputData: default
         );
 
-        _evmState = new EvmState(long.MaxValue, _environment, ExecutionType.TRANSACTION, true, _stateProvider.TakeSnapshot(), false);
+        _evmState = EvmState.RentTopLevel(long.MaxValue, ExecutionType.TRANSACTION, _stateProvider.TakeSnapshot(), _environment, new StackAccessTracker());
     }
 
     [Benchmark]
     public void ExecuteCode()
     {
-        var ts = _virtualMachine.Run<NotTracing>(_evmState, _stateProvider, _txTracer);
+        var ts = _virtualMachine.ExecuteTransaction<OffFlag>(_evmState, _stateProvider, _txTracer);
         if (ts.IsError)
         {
             throw new Exception("Execution failed: " + ts.Error);
